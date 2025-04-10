c240c477233753917935eb2a9316f6852cc89f77
Hovinhtu <hovinhtusqtt@gmail.com>
2025-04-10 09:37:21 +0700

    Save

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,7 +1,8 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
 import { getAuth } from "@clerk/nextjs/server";
-
+import { NextResponse } from "next/server";
+import User from "@/models/User";
 
 export async function POST(request) {
     try {
--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import User from "@/models/User";
 import { Inngest } from "inngest";
 import dbConnect from "./db";
+import Order from "@/models/Order";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
 
